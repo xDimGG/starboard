a3760f0f314b2edf332bcad63acf4416bc33c42b
Andrew Black <andrewblack2656@gmail.com>
2018-05-23 00:59:25 +0000

    Adapt to reviews

--- bots/starboard/bot/commands/configuration/selfstar.js
+++ bots/starboard/bot/commands/configuration/selfstar.js
@@ -3,16 +3,17 @@ const { Command } = require('../../../../../');
 class SelfStar extends Command {
 	constructor() {
 		super({
-			description: 'Toggles the ability to self-star.',
-			memberPerms: 'MANAGE_MESSAGES'
+			description: 'Toggles the self-star warning.',
+			memberPerms: 'MANAGE_MESSAGES',
 		});
 	}
-	async run(msg) {
-		const selfstar = this.client.settings.get(msg.guild.id, 'selfstar', true);
-		if (!msg.member.hasPermission('ADMINISTRATOR') && !msg.owner) return msg.channel.send(`Self-starring is currently ${selfstar ? 'enabled' : 'disabled'}.`);
-		await this.client.settings.set(msg.guild.id, 'selfstar', !selfstar);
 
-		return msg.channel.send(`Self-starring is now ${selfstar ? 'enabled' : 'disabled'}.`);
+	async run(msg) {
+		const selfStar = this.client.settings.get(msg.guild.id, 'selfstar', true);
+		if (!msg.member.hasPermission('ADMINISTRATOR') && !msg.owner) return msg.channel.send(`Self-star warnings are currently ${selfStar ? 'enabled' : 'disabled'}.`);
+		await this.client.settings.set(msg.guild.id, 'selfstar', !selfStar);
+
+		return msg.channel.send(`Self-star warnings now ${selfStar ? 'enabled' : 'disabled'}.`);
 	}
 }
 
